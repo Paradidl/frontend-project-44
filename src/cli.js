61b71b6a8e7ae321882316906834ea36c60ef9fc
Дмитрий Грелюк <d.grelyuk@gmail.com>
2023-01-24 00:06:35 +0700

    add src/

---
+++ src/cli.js
@@ -0,0 +1,5 @@
+import readlineSync from 'readline-sync';
+
+console.log ('Welcome to the Brain Games!');   
+const UserName = readlineSync.question('May I have your name? ');
+console.log ('Hello, '+ UserName + '!');
